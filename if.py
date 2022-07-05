bed82c3c223348174218e2acbc129c009c680ab4
吴高尚 <544685309@qq.com>
2022-07-05 19:51:53 +0800

    bugfix

--- if.py
+++ if.py
@@ -1,7 +1,7 @@
 age = 21
 if age<18:
     print("未成年")
-elif age>18 and age<=24:
+elif 18 < age <= 24:
     print("大学生活")
 else:
     print("成年工作生活")
--- oneday.py
+++ oneday.py
@@ -1,50 +1,46 @@
 print("第一个任务")
-age=71
+
 age = int(input("请猜猜我的年龄："))
 
-while age>0:
-    if age>71:
+while age > 0:
+    if age > 71:
         print("请往小了猜")
         age = int(input("请猜猜我的年龄："))
         continue
-    if age<71:
+    if age < 71:
         print("请往大了猜")
         age = int(input("请猜猜我的年龄："))
         continue
-    if age==71:
+    if age == 71:
         print("猜对了！")
         break
 
 print("第二个任务")
 while True:
     money = int(input("请输入月工资："))
-    if money<=1000:
+    if money <= 1000:
         print("老板，你自己玩吧")
-    elif money<=15000:
+    elif money <= 15000:
         print("老板，说的很好，但是我不听")
-    elif money<=20000:
+    elif money <= 20000:
         print("老板，你说的可以，可以一试下")
-    elif money<=30000:
+    elif money <= 30000:
         print("老板说啥都挺好，积极配合")
-    elif money<=50000:
+    elif money <= 50000:
         print("老板你一直对，不对也是对")
     else:
         print("公司就是我家")
         break
 
-
 # if age>71:
 #     print("我没有那么大！")
 #     continue
-# elif agtinuee<71:
-#     pritinuent("猜小了哈！")
+# elif age<71:
+#     print("猜小了哈！")
 #     continue
 # else:
 #     print("终于猜对了！")
 #     break
-
-
-
 
 
 # for i in age:
@@ -57,4 +53,3 @@ while True:
 #     while i==71:
 #         print("猜对了")
 #         break
-
